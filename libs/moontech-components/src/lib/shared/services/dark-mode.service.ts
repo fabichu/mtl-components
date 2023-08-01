b410efccfdefc087ddfae32801dde4e839dcea47
fferrerov <f.ferrero@moontech-industrial.com>
2023-08-01 12:11:24 +0200

    Refactor darkmode service

--- libs/moontech-components/src/lib/shared/services/dark-mode.service.ts
+++ libs/moontech-components/src/lib/shared/services/dark-mode.service.ts
@@ -2,9 +2,9 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable()
 export class DarkModeService {
-  public $mode = signal<boolean>(false)
-
-  public toogle (): void {
-    this.$mode.set(!this.$mode())
+  public mode = signal<boolean>(false)
+  
+  toogle (): void {
+    this.mode.set(!this.mode())
   }
 }
