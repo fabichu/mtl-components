93078ef302ba3a54a4c7524005916f923a812f14
MArandaB <MArandaBarca@gmail.com>
2023-08-16 09:12:30 +0200

    ADD enum

---
+++ libs/moontech-components/src/lib/components/mtl-badge/types/mtl-badge.ts
@@ -0,0 +1,12 @@
+export enum MtlBadgeType {
+    INFO = 'info',
+    WARNING = 'warning',
+    SUCCESS = 'success',
+    DANGER = 'danger',
+}
+
+export enum MtlBadgeSize {
+    BIG = 'big',
+    MEDIUM = 'medium',
+    SMALL = 'small',
+}
